46e66ee2a79f537327e95488f6840b91af31afe2
Vishal Yadav <ydvishal89@gmail.com>
2026-04-03 10:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let form = document.getElementById("myForm");
+form.addEventListener("submit"  , function(e){
+	e.preventDefault();
+
+	let fname = document.getElementById("fname").value;
+	let lname = document.getElementById("lname").value;
+	let number = document.getElementById("pnumber").value;
+	let mail  = document.getElementById("mail").value;
+
+	let message = "First Name:" + fname + " " +"Last Name:" +lname +" "+"Phone Number:"+number +" "+"Email ID:"+mail +" "
+	alert(message);
+});
